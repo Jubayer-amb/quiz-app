b65ad5f34e2ede7af8f9e898786675ee1f85b1a9
Jubayer Al Mamun <jubayeramb@gmail.com>
2021-11-22 00:02:54 +0600

    userName and logout function added

--- src/components/navbar/Account.jsx
+++ src/components/navbar/Account.jsx
@@ -1,18 +1,39 @@
 import { NavLink } from 'react-router-dom';
+import { useAuth } from '../../contexts/AuthContext';
 import classes from '../../styles/Account.module.css';
 
-const Account = () => (
-    <div className={classes.account}>
-        <div className={classes.avatar}>
-            <span className="material-icons-outlined"> account_circle </span>
+const Account = () => {
+    const { currentUser, signout } = useAuth();
+    return (
+        <div className={classes.account}>
+            <div className={classes.avatar}>
+                <span className="material-icons-outlined"> account_circle </span>
+            </div>
+            {currentUser ? (
+                <>
+                    <span>{currentUser.displayName}</span>
+                    <span
+                        className="material-icons-outlined"
+                        onClick={signout}
+                        role="button"
+                        tabIndex="0"
+                    >
+                        {' '}
+                        logout{' '}
+                    </span>
+                </>
+            ) : (
+                <>
+                    <NavLink className={classes.anchor} to="/signup">
+                        <span>Signup</span>
+                    </NavLink>
+                    <NavLink className={classes.anchor} to="/login">
+                        <span>Login</span>
+                    </NavLink>
+                </>
+            )}
         </div>
-        <NavLink className={classes.anchor} to="/signup">
-            <p>Signup</p>
-        </NavLink>
-        <NavLink className={classes.anchor} to="/login">
-            <p>Login</p>
-        </NavLink>
-    </div>
-);
+    );
+};
 
 export default Account;
