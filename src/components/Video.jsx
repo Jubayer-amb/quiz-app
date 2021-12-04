8955f8d11b1d13a7f495a8c8b28e8c146208ffa4
Jubayer Al Mamun <jubayeramb@gmail.com>
2021-12-05 02:16:55 +0600

    Dynamic video fetching from Realtime DB using useVideoList hook

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,19 +1,22 @@
 import { useNavigate } from 'react-router-dom';
-import thumbnail from '../assets/img/3.jpg';
 import classes from '../styles/Video.module.css';
 
-const Video = () => {
+const Video = ({ title, id, noq }) => {
     const navigate = useNavigate();
     const redirect = () => {
         navigate('/quiz');
     };
     return (
         <div className={classes.video} onClick={redirect} aria-hidden>
-            <img src={thumbnail} alt="Thumbnail" className={classes.thumbnail} />
-            <p>#23 React Hooks Bangla - React useReducer hook Bangla</p>
+            <img
+                src={`https://img.youtube.com/vi/${id}/maxresdefault.jpg`}
+                alt="Thumbnail"
+                className={classes.thumbnail}
+            />
+            <p>{title}</p>
             <div className={classes.qmeta}>
-                <p>10 Questions</p>
-                <p>Score: 10 / 10</p>
+                <p>{noq} Questions</p>
+                <p>Total Points: {noq * 5}</p>
             </div>
         </div>
     );
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,17 +1,33 @@
+import { useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
+import useVideosList from '../hooks/useVideosList';
 import classes from '../styles/Videos.module.css';
 import Video from './Video';
 
-const Videos = () => (
-    <div className={classes.videos}>
-        <Video />
-        <Video />
-        <Video />
-        <Video />
-        <Video />
-        <Video />
-        <Video />
-        <Video />
-    </div>
-);
+const Videos = () => {
+    const [pageLimit, setPageLimit] = useState(0);
+    const { loading, error, videos, hasMore } = useVideosList(pageLimit);
+    return (
+        <>
+            {videos.length !== 0 && (
+                <InfiniteScroll
+                    dataLength={videos.length}
+                    hasMore={hasMore}
+                    next={() => setPageLimit(pageLimit + 9)}
+                    className={classes.videos}
+                >
+                    {videos.map((video) => (
+                        <div key={video.youtubeID}>
+                            <Video title={video.title} id={video.youtubeID} noq={video.noq} />
+                            {!loading && videos.length === 0 && <div> No videos found! </div>}
+                            {loading && <div> Loading... </div>}
+                            {error && <div> There was an error! </div>}
+                        </div>
+                    ))}
+                </InfiniteScroll>
+            )}
+        </>
+    );
+};
 
 export default Videos;
